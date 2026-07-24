de4a5aef0141e76a9323211cc3b5285e59bb2432
syedsalman23 <salmansalu2757@gmail.com>
2026-07-24 17:40:59 +0530

    update dockerfile & docker-compose.yml

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb"
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME
 };
